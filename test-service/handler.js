5dcef2000918b3b0824ac5d61cc01cfb10603e0c
Retama <edmundo.martinez@ipointsystems.com>
2019-04-02 19:49:41 -0600

    initial lambda project

---
+++ test-service/handler.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const region = 'us-east-1';
+
+let sqs = new AWS.SQS({region: region});
+
+const AWS_ACCOUNT = process.env.ACCOUNT_ID;
+//const QUEUE_URL = `https://sqs.${region}.amazonaws.com/${AWS_ACCOUNT}/MyQueue`;
+const QUEUE_URL = `https://sqs.us-east-1.amazonaws.com/928410696171/MyQueue`;
+
+module.exports.hello = async (event, context, callback) => {
+
+  let response = {};
+
+  //console.log(event, context, callback);
+
+  const params = {
+    MessageBody: 'Hola',
+    QueueUrl: QUEUE_URL
+  };
+
+  console.log('before the storm');
+  try{
+    sqs.sendMessage(params, function(err, data) {
+
+      console.log('has error?', err);
+      if(err){
+        console.log('error', 'message cannot be sent: ' + err);
+        response = {
+          statusCode: 500,
+          body: JSON.stringify({
+            message: err
+          })
+        };
+      }else{
+        console.log('data: ', data);
+        response = {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: data.MessageId
+          })
+        };
+      }
+      callback( response);
+  
+    });
+  }catch(err){
+    console.log('error Vergas', err);
+  }
+  
+
+  
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.sqsHello = async (event, context) => {
+  console.log('sqs hello called');
+  console.log(event);
+
+  context.done(null, '');
+};
